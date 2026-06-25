01561e4cbb728ceab714c0a0fd05a1ffa92d36b3
POOVIZHI P <poovizhip.23csd@kongu.edu>
2026-06-25 12:00:39 +0530

    Time: 1041 ms (22.41%), Space: 0B (100%) - LeetHub

---
+++ 0184-department-highest-salary/0184-department-highest-salary.sql
@@ -0,0 +1,1 @@
+select d.name as Department ,e1.name as Employee ,e1.salary as Salary from Employee e1 join Department d on e1.departmentId = d.id where e1.salary =(select max(salary) from Employee where departmentId =e1.departmentId);
