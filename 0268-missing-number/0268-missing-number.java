9107100daf731cd2cf6585467b89516ebf2e0d20
POOVIZHI P <poovizhip.23csd@kongu.edu>
2026-08-11 21:34:21 +0530

    Time: 0 ms (100%), Space: 47.3 MB (66.73%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -2,7 +2,7 @@ class Solution {
     public int missingNumber(int[] nums) {
         int xor=nums.length;
         for(int i=0;i<nums.length;i++){
-            xor=xor^i^nums[i];
+                xor=xor^i^nums[i];
         }
         return xor;
     }
