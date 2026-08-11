40700b904eb7c6d1f4743c38780dbdf1f6c1a6c4
POOVIZHI P <poovizhip.23csd@kongu.edu>
2026-08-11 21:28:13 +0530

    Time: 1904 ms (5.11%), Space: 55.9 MB (6.81%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,20 +1,16 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        int candidate=0;
-        int count=0;
-        for(int num: nums){
-            if(count==0){
-                candidate=num;
-            }
-        if(candidate==num){
-                count++;
-            }
-    
-            else{
-                count--;
-            }
-
+  for(int i=0;i<nums.length;i++){
+    int count=0;
+    for(int j=0;j<nums.length;j++){
+        if(nums[i]==nums[j]){
+            count++;
         }
-        return candidate;
+    }
+    if(count>nums.length/2){
+        return nums[i];
+    }
+  }
+  return -1;
     }
 }
