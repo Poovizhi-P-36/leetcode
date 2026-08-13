a97edca1c0561021733d01da7cc39df9865d3ce1
POOVIZHI P <poovizhip.23csd@kongu.edu>
2026-08-13 11:40:00 +0530

    Time: 1 ms (11.79%), Space: 43.6 MB (59.18%) - LeetHub

---
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int findMiddleIndex(int[] nums) {
+        for(int i=0;i<nums.length;i++){
+            int left=0;
+            int right=0;
+            for(int j=0;j<i;j++){
+                left=left+nums[j];
+            }
+                for(int j=i+1;j<nums.length;j++){
+                    right=right+nums[j];
+                }
+        
+                if(left==right){
+                  return i;
+                }
+            }
+            return -1;
+        }
+    }
+
